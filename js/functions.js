7e5ceec15d020ffb30956aa651587def8995fa3f
daniel.arrieta <dano56ace@gmail.com>
2022-04-20 00:34:37 -0400

    Fixed Form rev

--- js/functions.js
+++ js/functions.js
@@ -35,7 +35,7 @@ $(document).ready (function() {
 		function processResults(data, textStatus) {
 			if(textStatus == 'success')
 			{
-				$('#success').text('<p class="regular">The message has been sent successfully</p>');
+				$('#success').text('The message has been sent successfully!');
 				$('#success').fadeIn('slow'); //display success message
 				$('#email').hide(); //hide form
 			}
